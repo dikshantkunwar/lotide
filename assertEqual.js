0a4c38db8c8baeb7f6c3e216d51fb7b09df19450
dikshantkunwar <dikshantkunwar@gmail.com>
2021-11-12 19:34:31 +0000

    refactoring test code from head.js and assertEquals.js

--- assertEqual.js
+++ assertEqual.js
@@ -7,6 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -2,13 +2,4 @@ const head = function(arr){
   return arr[0];
 }
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("😀 😀 Assertion Passed: " + actual + " === " + expected);
-  } else {
-    console.log("🥵 🥵 Assertion Failed: " + actual + " !== " + expected);
-  }
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
