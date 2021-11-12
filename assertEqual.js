6166fc13488fae2a9793c5f9f67d23d3a2664d8e
dikshantkunwar <dikshantkunwar@gmail.com>
2021-11-12 15:54:57 +0000

    implemented assertEquals function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀 😀 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🥵 🥵 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
