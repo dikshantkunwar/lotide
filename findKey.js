0431a78a265979572dc59e41e61f4b07449dd443
dikshantkunwar <dikshantkunwar@gmail.com>
2021-11-16 17:48:47 +0000

    implemented findKey function

---
+++ findKey.js
@@ -0,0 +1,46 @@
+
+const assertEqual = require('./assertEqual');
+
+const findKey = (obj, callback) => {
+  let objValues = Object.keys(obj);
+
+  for (const val of objValues) {
+    if (callback(obj[val])) {
+      return val;
+    }
+  }
+};
+
+// Test Assertions
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(result1,"noma");
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5);
+
+assertEqual(result2,undefined);
+
+const result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+assertEqual(result3,"Akaleri");
