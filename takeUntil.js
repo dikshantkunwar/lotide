8ffefc6382ec796c85816cdb743aeb8645618c33
dikshantkunwar <dikshantkunwar@gmail.com>
2021-11-16 17:28:33 +0000

    added takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,29 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+
+    if (callback(item)) {
+      break;
+    } else {
+      results.push(item);
+    }
+  }
+
+  return results;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+// Test Assertions
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
