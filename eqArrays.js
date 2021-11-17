349385e19cb0d3a6e6f78f344ae9150fbe1d6c49
dikshantkunwar <dikshantkunwar@gmail.com>
2021-11-17 18:07:58 +0000

    refactor some test codes

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("😀 😀 Assertion Passed: " + actual + " === " + expected);
-  } else {
-    console.log("🥵 🥵 Assertion Failed: " + actual + " !== " + expected);
-  }
-};
-
 const eqArrays = (arr1, arr2) => {
   if (arr1.length != arr2.length){
     return false;
@@ -21,4 +13,3 @@ const eqArrays = (arr1, arr2) => {
 
 module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
---
+++ test/eqArraysTest.js
@@ -0,0 +1,4 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
