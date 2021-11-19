f74696a756b2f9a6e62bb6a593147f96ea159c93
dikshantkunwar <dikshantkunwar@gmail.com>
2021-11-19 17:18:37 -0500

    refactoring middle.js tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-// TEST/ASSERTION FUNCTIONS
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-//TEST CODE
-assertArraysEqual(middle(["i", "am", "in", "middle", "here", "guys"]), ["in", "middle"]);
-assertArraysEqual(middle([1,2]),[]);
-assertArraysEqual(middle(["one", 2, "chess", "four", 4]), ["chess"]);
+describe("#middle", () => {
+  it("returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+
+  it("returns [3,4] for [1,2,3,4,5,6]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
+  });
+
+  it("returns empty array [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+
+  it("returns ['middle'] for ['front', 'side', 'middle', 'side', 'end']", () => {
+    assert.deepEqual(middle(['front', 'side', 'middle', 'side', 'end']), ['middle']);
+  });
+});
