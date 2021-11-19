22abbeaf956b23eceac86f3d2c699c5371a3c183
dikshantkunwar <dikshantkunwar@gmail.com>
2021-11-19 12:20:30 -0500

    introduced tests with mocha using chai assertions

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,12 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-//TESTS
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1,2,3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
